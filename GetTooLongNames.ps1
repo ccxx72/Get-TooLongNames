f34b2f068a9eb34aca6b28799d7062869b73eb37
Giuliano Pisoni <ccxx.72@gmail.com>
2022-12-22 12:43:44 +0100

    Add files via upload

---
+++ GetTooLongNames.ps1
@@ -0,0 +1,17 @@
+﻿function Get-TooLongNames {
+    param(
+        [string]$Path,
+        [string]$OutputFile
+    )
+
+
+    Get-ChildItem $Path -Recurse | foreach {
+       if ($_.FullName.Length -gt 250) {
+            Write-Host "$($_.Name)  -  $($_.FullName.Length)" 
+           $_ | Out-file $OutputFile -Append
+           "Path Lenght " + $_.FullName.Length | Out-file $OutputFile -Append
+        }
+    }
+}
+
+Get-TooLongNames -Path "PATH" -OutputFile "FILENAME"
