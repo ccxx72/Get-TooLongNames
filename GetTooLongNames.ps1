29b0b23056a0e77dc8dbf8e77dec229e856b93ad
Giuliano Pisoni <ccxx.72@gmail.com>
2023-09-29 16:10:47 +0200

    Update GetTooLongNames.ps1

--- GetTooLongNames.ps1
+++ GetTooLongNames.ps1
@@ -1,17 +1,29 @@
-﻿function Get-TooLongNames {
+function Get-TooLongNames {
     param(
         [string]$Path,
         [string]$OutputFile
     )
 
+    # Creare un array per i risultati
+    $results = @()
 
     Get-ChildItem $Path -Recurse | foreach {
        if ($_.FullName.Length -gt 250) {
-            Write-Host "$($_.Name)  -  $($_.FullName.Length)" 
-           $_ | Out-file $OutputFile -Append
-           "Path Lenght " + $_.FullName.Length | Out-file $OutputFile -Append
+            Write-Host "$($_.Name) - $($_.FullName.Length)" 
+            $result = [PSCustomObject]@{
+                Name = $_.Name
+                Path = $_.FullName
+                Length = $_.FullName.Length
+            }
+
+            # Aggiungere il risultato all'array
+            $results += $result
         }
     }
+
+    # Esportare i risultati in un file CSV
+    $results | Export-Csv -Path $OutputFile -NoTypeInformation
 }
 
+# Chiamare la funzione e specificare il percorso e il file di output
 Get-TooLongNames -Path "PATH" -OutputFile "FILENAME"
